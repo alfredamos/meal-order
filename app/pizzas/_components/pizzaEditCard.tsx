650faa66394dd696f7e07adb4f1b63f4676323ad
alfredamos <alfredamos@gmail.com>
2025-01-01 15:00:28 -0500

    refactor pizza-edit card to use action in place of onSubmit

--- app/pizzas/_components/pizzaEditCard.tsx
+++ app/pizzas/_components/pizzaEditCard.tsx
@@ -4,7 +4,6 @@ import PizzaInputForm from "@/components/pizza/pizzaInputForm.form";
 import { Pizza } from "@prisma/client";
 import { FormEvent } from "react";
 import ClientCancelButton from "./clientCancelButton";
-import toast from "react-hot-toast";
 
 type Props = {
   pizza: Pizza;
@@ -18,12 +17,8 @@ export default function PizzaEditCard({
   onEdit,
   formName,
 }: Props) {
-  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const editedPizza = Object.fromEntries(
-      formData.entries()
-    ) as unknown as Pizza;
+  const submitHandler = (formData: FormData) => {
+    const editedPizza = Object.fromEntries(formData) as unknown as Pizza;
 
     onEdit(editedPizza);
   };
@@ -32,7 +27,7 @@ export default function PizzaEditCard({
     <form
       method="PATCH"
       className="bg-white text-slate-800 flex flex-col justify-center items-center mx-auto rounded-xl shadow-2xl py-10"
-      onSubmit={(event) => submitHandler(event)}
+      action={submitHandler}
     >
       <PizzaInputForm formName={formName} pizza={pizza}>
         <ClientCancelButton onCancel={onCancel} />
